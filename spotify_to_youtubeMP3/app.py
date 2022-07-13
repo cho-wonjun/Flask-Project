a32b3853ea4393d373e4b4863cfbf0babf1a91ab
Won Jun Cho <wonjun2004@gmail.com>
2022-07-12 22:01:25 -0700

    Update app.py

--- spotify_to_youtubeMP3/app.py
+++ spotify_to_youtubeMP3/app.py
@@ -4,7 +4,7 @@ from flask import Flask, url_for, session, request, redirect
 import time
 import pandas as pd
 
-# App config
+
 app = Flask(__name__)
 
 app.secret_key = 'something-special'
@@ -58,21 +58,21 @@ def get_all_tracks():
     return "done"
 
 
-# Checks to see if token is valid and gets a new token if not
+# Check if token is valid and gets a new token if not
 def get_token():
     token_valid = False
     token_info = session.get("token_info", {})
 
-    # Checking if the session already has a token stored
+    # Check if the session already has a token stored
     if not (session.get('token_info', False)):
         token_valid = False
         return token_info, token_valid
 
-    # Checking if token has expired
+    # Check if token has expired
     now = int(time.time())
     is_token_expired = session.get('token_info').get('expires_at') - now < 60
 
-    # Refreshing token if it has expired
+    # Refresh token if it expired
     if (is_token_expired):
         sp_oauth = create_oauth()
         token_info = sp_oauth.refresh_access_token(session.get('token_info').get('refresh_token'))
